89ac5dca147dd3aaa6de9903623300a56a438469
Lycan-Chain <team@lycanchain.com>
2022-09-03 20:02:17 +0530

    Config changes.

--- config/dev.exs
+++ config/dev.exs
@@ -8,3 +8,22 @@ config :logger, :ecto,
   path: Path.absname("logs/dev/ecto.log")
 
 config :logger, :error, path: Path.absname("logs/dev/error.log")
+
+config :block_scout_web, BlockScoutWeb.Endpoint,
+    http: [port: 4000],
+    https: [
+      port: 4001,
+      cipher_suite: :strong,
+      certfile: "priv/cert/selfsigned.pem",
+      keyfile: "priv/cert/selfsigned_key.pem"
+    ]
+
+config :explorer, Explorer.Repo,
+  database: "explorer_dev",
+  hostname: "localhost",
+  username: "postgres",
+  password: "bscoutpass",
+  pool_size: 200,
+  timeout: 800_000,
+  queue_target: 50000
+
